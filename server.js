1637bc3ce2f59efb721070bb3770d982071df49b
HamzaA4E <herohamza24@gmail.com>
2025-02-16 12:37:37 +0100

    installing socket.io

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const app = express();
 const http = require('http')
 const path = require('path')
-const port = 4000;
+const socketio = require('socket.io')
 
+const port = 4000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//Set static folder
 app.use(express.static(path.join(__dirname)))
-app.listen(port, () =>
+
+//Run when a client connect
+io.on('connection', (socket)=>
+{
+    console.log("A user has enter the chat")
+})
+
+
+server.listen(port, () =>
 {
     console.log(`Your app is running at http://localhost:${port}`)
 })
